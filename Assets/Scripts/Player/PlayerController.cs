44896aa13bccac9101129d2b9b10a4a4ea91115f
CroSparks <jvyaris@gmail.com>
2016-10-29 13:36:13 -0700

    [Animations]
    - Added new animation flow
    - Added new player sprites
    - Scripted controls for animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,26 +4,128 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     //variables
-    public float            moveSpeed = 0;
-    public float            xMovement;//test input
-    public float            yMovement;//test input
+    public float            maxMoveSpeed    = 5;
+    public float            moveSpeed       = 5;
+    public float            jumpPower       = 5;
+    public float            xMovement;         
+    public float            yMovement;
+    private bool             isGrounded;
+    private bool            isCrouching;
+
     //components
     private Animator        myAnimator;
     private Rigidbody2D     myRigidbody;
+    private SpriteRenderer  myRenderer;
 
     //prefabs
 
     void Start()
     {
+        //get components
         myAnimator      = GetComponent<Animator>();
         myRigidbody     = GetComponent<Rigidbody2D>();
+        myRenderer      = GetComponent<SpriteRenderer>();
+
+        //init vars
+        moveSpeed = maxMoveSpeed;
     }
     
     void Update()
     {
+        ControlIn();
+        FlipSprite();
+
+
+    }
+    void FlipSprite()
+    {
+        if (xMovement < 0)
+        {
+            myRenderer.flipX = true;
+        }
+        if (xMovement > 0)
+        {
+            myRenderer.flipX = false;
+        }
+    }
+    void ControlIn()
+    {
+        //get xy input
         xMovement = Input.GetAxisRaw("Horizontal");
         yMovement = Input.GetAxisRaw("Vertical");
+        
+        //move left or right
+        myRigidbody.velocity = new Vector2(xMovement * moveSpeed, myRigidbody.velocity.y);
 
-        //myRigidbody.velocity = new Vector2(xMovement * moveSpeed, myRigidbody.velocity.y);
+        //attack on ground
+        if (Input.GetKeyDown(KeyCode.J) && isGrounded)
+        {
+            myAnimator.SetTrigger("regAttack!");
+            
+        }
+        if (myAnimator.GetCurrentAnimatorStateInfo(0).IsName("RegAttack"))
+        {
+            moveSpeed = 0;
+        }
+        else
+        {
+            moveSpeed = maxMoveSpeed;
+        }
+        //Air controls
+        AirIn();
+
+        //Crounching
+        CrouchingIn();
+
+        //set animations
+        myAnimator.SetFloat("xVelocity", Mathf.Abs(xMovement));
+        myAnimator.SetFloat("yVelocity", myRigidbody.velocity.y);
+        myAnimator.SetBool("isGrounded", isGrounded);
+        myAnimator.SetBool("isCrouching", isCrouching);
+    }
+
+    void AirIn()
+    {
+        //jump
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            myAnimator.SetTrigger("jump!");
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpPower);
+        }
+    }
+    void CrouchingIn()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            isCrouching = true;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            isCrouching = false;
+        }
+
+        if(isCrouching)
+        {
+            moveSpeed = 0;
+        }
+        else
+        {
+            moveSpeed = maxMoveSpeed;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
     }
 }
