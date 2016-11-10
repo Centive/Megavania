a3d92d76eb3c931beaaf55dd52d993458ace9b0f
CroSparks <jvyaris@gmail.com>
2016-11-10 07:27:07 -0800

    Added new controls and animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,20 +4,24 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     //variables
-    public float            maxMoveSpeed    = 5;
-    public float            moveSpeed       = 5;
-    public float            jumpPower       = 5;
+    public float            maxMoveSpeed    = 5f;
+    public float            moveSpeed       = 5f;
+    public float            jumpPower       = 5f;
     public float            xMovement;         
     public float            yMovement;
-    private bool             isGrounded;
-    private bool            isCrouching;
+    public bool             isGrounded;
+    public bool             isCrouching;
+    public bool             isFlying;
+    public bool             isAttacking;
+    public bool             isFacingLeft    = false;
 
     //components
-    private Animator        myAnimator;
-    private Rigidbody2D     myRigidbody;
-    private SpriteRenderer  myRenderer;
+    public Animator         myAnimator;
+    public Rigidbody2D      myRigidbody;
+    public SpriteRenderer   myRenderer;
 
     //prefabs
+    public GameObject       knifePrefab;
 
     void Start()
     {
@@ -27,85 +31,132 @@ public class PlayerController : MonoBehaviour
         myRenderer      = GetComponent<SpriteRenderer>();
 
         //init vars
-        moveSpeed = maxMoveSpeed;
+        moveSpeed       = maxMoveSpeed;
+        isFlying        = false;
     }
     
     void Update()
     {
-        ControlIn();
-        FlipSprite();
-
-
+        CancelMovements();
+        FlyIn();
+        GroundIn();
+        AirIn();
+        CrouchingIn();
+        if (!isAirRegAttack() && !isCrouchRegAttack() && !isRegAttack())
+        {
+            FlipSprite();
+        }
     }
+
     void FlipSprite()
     {
         if (xMovement < 0)
         {
-            myRenderer.flipX = true;
+            Vector3 currentScale = transform.localScale;
+            currentScale.x = -1;
+            transform.localScale = currentScale;
+            isFacingLeft = true;
         }
         if (xMovement > 0)
         {
-            myRenderer.flipX = false;
+            Vector3 currentScale = transform.localScale;
+            currentScale.x = 1;
+            transform.localScale = currentScale;
+            isFacingLeft = false;
         }
-    }
-    void ControlIn()
+    }      // Flips the gameobject
+    void GroundIn()
     {
         //get xy input
-        xMovement = Input.GetAxisRaw("Horizontal");
-        yMovement = Input.GetAxisRaw("Vertical");
+        xMovement = (Input.GetAxisRaw("Horizontal") * moveSpeed);
+        yMovement = (isFlying) ? (Input.GetAxisRaw("Vertical") * moveSpeed) : myRigidbody.velocity.y;
         
         //move left or right
-        myRigidbody.velocity = new Vector2(xMovement * moveSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = new Vector2(xMovement, yMovement);
 
         //attack on ground
-        if (Input.GetKeyDown(KeyCode.J) && isGrounded)
+        if (Input.GetButtonDown("SquareButton") && isGrounded && !isCrouching && !isRegAttack())
         {
             myAnimator.SetTrigger("regAttack!");
-            
         }
-        if (myAnimator.GetCurrentAnimatorStateInfo(0).IsName("RegAttack"))
-        {
-            moveSpeed = 0;
-        }
-        else
-        {
-            moveSpeed = maxMoveSpeed;
-        }
-        //Air controls
-        AirIn();
 
-        //Crounching
-        CrouchingIn();
+        //knife
+        if(Input.GetButtonDown("RightBumper"))
+        {
+            Instantiate(knifePrefab, transform.position + new Vector3(-0.3f, 0.0f, 0.0f), (Quaternion.identity));
+        }
 
         //set animations
-        myAnimator.SetFloat("xVelocity", Mathf.Abs(xMovement));
+        myAnimator.SetFloat("xVelocity", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
         myAnimator.SetFloat("yVelocity", myRigidbody.velocity.y);
         myAnimator.SetBool("isGrounded", isGrounded);
-        myAnimator.SetBool("isCrouching", isCrouching);
-    }
-
+    }        // Controls available in the ground
     void AirIn()
     {
         //jump
-        if (Input.GetKeyDown(KeyCode.K) && !isCrouching)
+        if (Input.GetButtonDown("XButton") && !isCrouching && isGrounded)
         {
             myAnimator.SetTrigger("jump!");
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpPower);
         }
-    }
+        
+        if(Input.GetButtonDown("SquareButton") && !isGrounded)
+        {
+            myAnimator.SetTrigger("airRegAttack!");
+        }
+    }           // Controls available in the air
     void CrouchingIn()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+        //set if player is crouching
+        if(!isFlying)
         {
-            isCrouching = true;
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            isCrouching = false;
+            if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                isCrouching = true;
+            }
+            else
+            {
+                isCrouching = false;
+            }
         }
 
-        if(isCrouching)
+        if (isCrouching)//actions while crouching
         {
+            if (Input.GetButtonDown("SquareButton") && !isCrouchRegAttack())//crouching attack
+            {
+                myAnimator.SetTrigger("crouchAttack!");
+            }
+        }
+        //set animations
+        myAnimator.SetBool("isCrouching", isCrouching);
+    }     // Controls available while crouching
+    void FlyIn()
+    {
+        if(!isGrounded)
+        {
+            if(Input.GetButtonDown("XButton"))
+            {
+                isFlying = true;
+            }
+        }
+        if(isFlying)
+        {
+            myRigidbody.isKinematic = true;
+            if (Input.GetButtonUp("XButton"))
+            {
+                isFlying = false;
+            }
+        }
+        else
+        {
+            myRigidbody.isKinematic = false;
+        }
+    }           // Controls available for flying
+    void CancelMovements()
+    {
+        if (isRegAttack() || isCrouching)
+        {
+            //myRigidbody.constraints = RigidbodyConstraints2D.FreezePositionY;
             moveSpeed = 0;
         }
         else
@@ -114,14 +165,28 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    //Check if attack animations are playing
+    bool isRegAttack()
+    {
+        return (myAnimator.GetCurrentAnimatorStateInfo(0).IsName("RegAttack"));
+    }
+    bool isAirRegAttack()
+    {
+        return (myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Air_RegAttack"));
+    }
+    bool isCrouchRegAttack()
+    {
+        return (myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Crouch_RegAttack"));
+    }
+
     void OnCollisionStay2D(Collision2D col)
     {
-        Debug.Log("eyy");
         if(col.gameObject.tag == "Ground")
         {
             isGrounded = true;
         }
     }
+
     void OnCollisionExit2D(Collision2D col)
     {
         if (col.gameObject.tag == "Ground")
---
+++ Assets/Scripts/PlayerKnife.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerKnife : MonoBehaviour {
+
+    public GameObject player;
+    public float bulletSpeed = 12.0f;
+    public float killBulletTimer = 0.2f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        bulletSpeed = player.GetComponent<PlayerController>().isFacingLeft ? bulletSpeed * -1.0f : bulletSpeed;
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(bulletSpeed * Time.deltaTime, 0.0f);
+
+        pos += transform.rotation * velocity;
+        transform.position = pos;
+
+
+        killBulletTimer -= Time.deltaTime;
+        if (killBulletTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
