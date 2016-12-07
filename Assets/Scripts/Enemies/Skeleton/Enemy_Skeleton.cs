1fdc77af747ff8b19d432026fd8d9f226c19190e
CroSparks <jvyaris@gmail.com>
2016-12-06 17:28:54 -0800

    Organized layers for collisions, and fixed mr skeleton's passive state
    - Added additional layers for enemies, projectiles, and player to ignore
    some collisions and raycasting to detect the player layer
    - Passive state now moves mr skeleton back and forth
    - added a new bullet for the aggressive state to shoot(still inprogress)

--- Assets/Scripts/Enemies/Skeleton/Enemy_Skeleton.cs
+++ Assets/Scripts/Enemies/Skeleton/Enemy_Skeleton.cs
@@ -12,11 +12,18 @@ public class Enemy_Skeleton : MonoBehaviour
 
     public float maxVisionRange = 0f;
     public float curVisionRange = 0f;
+    public float curWalkingDist = 0f;
     public float maxWalkingDist = 0f;
-
     public float maxSpeed = 0f;
     public float curSpeed = 0f;
+
+    public float nextFire = 0f;
+    public float fireRate = 3f;
     public WalkDir curDirection = WalkDir.Forth;
+    public GameObject prefabBullet;
+
+    private GameObject playerTarget;
+    private Rigidbody2D myRigidbody;
 
     // Use this for initialization
     void Start()
@@ -24,28 +31,76 @@ public class Enemy_Skeleton : MonoBehaviour
         //init variables
         curVisionRange = maxVisionRange;
         curSpeed = maxSpeed;
+        curWalkingDist = maxWalkingDist;
+
+        myRigidbody = GetComponent<Rigidbody2D>();
+        playerTarget = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //update aggressive
+        //update Aggressive actions
         if (IsAggressive())
         {
-            Debug.Log("is aggressive");
+            Debug.Log("AGGRESSIVE");
+            if (Time.time > nextFire)
+            {
+                Debug.Log("SHOOT");
+                nextFire = Time.time + fireRate;
+
+                GameObject obj = Instantiate(prefabBullet, transform.position, Quaternion.identity) as GameObject;
+                Debug.Log(obj.name + " is created");
+            }
+
         }
+        //Update Passive actions
         else
         {
-            Debug.Log("is Passive");
+            Vector3 currentScale = transform.localScale;
+            myRigidbody.velocity = new Vector2(1 * curSpeed, myRigidbody.velocity.y);
+            //Flip the x sprite moving left or right
+            if (curDirection == WalkDir.Back)
+            {
+                currentScale.x = -0.1f;
+                transform.localScale = currentScale;
+            }
+
+            if (curDirection == WalkDir.Forth)
+            {
+                currentScale.x = 0.1f;
+                transform.localScale = currentScale;
+            }
+
+            //Move back and forth
+            curWalkingDist -= Time.deltaTime;
+            if (curWalkingDist < 0)
+            {
+                if (curDirection == WalkDir.Forth)
+                {
+                    curDirection = WalkDir.Back;
+                    curSpeed *= -1f;
+                }
+                else
+                {
+                    curDirection = WalkDir.Forth;
+                    curSpeed *= -1f;
+                }
+                curWalkingDist = maxWalkingDist;
+            }
         }
     }
 
     bool IsAggressive()
     {
+
         Vector2 visionEnd = new Vector2(transform.position.x + curVisionRange, transform.position.y);
 
+        if (curSpeed < 0f)
+            visionEnd.x = transform.position.x + curVisionRange * -1;
+
         //Returns if linecast hits something
-        RaycastHit2D hit = Physics2D.Linecast(transform.position, visionEnd, LayerMask.GetMask("Enemy_Visions"));
+        RaycastHit2D hit = Physics2D.Linecast(transform.position, visionEnd, LayerMask.GetMask("Player"));
 
         Debug.DrawLine(transform.position, visionEnd, Color.red);
 
---
+++ Assets/Scripts/Enemies/Skeleton/Skeleton_Bullet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skeleton_Bullet : MonoBehaviour
+{
+    public float throwPowerY = 0f;
+    public float throwPowerX = 0f;
+    public float curKillTimer = 0f;
+    public float maxKillTimer = 0f;
+
+    public Rigidbody2D myRigidbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        curKillTimer = maxKillTimer;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myRigidbody.AddForce(new Vector2(throwPowerX, throwPowerY), ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Kill bullet
+        curKillTimer -= Time.deltaTime;
+        if (curKillTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
